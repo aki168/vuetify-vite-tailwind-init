52b4dda505c04315e0c6a96a5331be583713b163
BECREATOR/aki.zheng <ahsi.aki@gmail.com>
2022-12-16 11:33:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <!-- <HelloWorld /> -->
+      <v-alert color="info" icon="$info">
+        I'm an info alert that uses <strong>color</strong> and <strong>icon</strong> props instead of
+        <strong>type</strong>
+      </v-alert>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
