3c60daa97f1b879399b33450fe323ff34e121e0d
BECREATOR/aki.zheng <ahsi.aki@gmail.com>
2022-12-21 11:55:40 +0800

    script setup

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import ChatRoom from '@/components/ChatRoom.vue';
+
+const counter = ref(10)
+const add = () => { counter.value++ }
+
+onMounted(() => {
+  console.log(`初始值是${counter.value}`)
+})
+
+// const drawer = null
+</script>
+
 <template>
   <v-app>
-    <ChatRoom/>
+    <ChatRoom />
+    <!-- <div class="py-3 flex flex-col">
+      <p class="text-overline text-center">{{ counter }}</p>
+      <v-btn @click="add" variant="flat" color="primary" class="w-50 mx-auto">
+        Click Me!
+      </v-btn>
+    </div> -->
+
   </v-app>
 </template>
-
-<script>
-// import HelloWorld from '@/components/HelloWorld.vue'
-import ChatRoom from '@/components/ChatRoom.vue';
-export default {
-    data: () => ({ drawer: null }),
-    components: { ChatRoom }
-}
-</script>
